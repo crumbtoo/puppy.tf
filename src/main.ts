936d14a7c1eac953100d786664ab378445f8f4dd
Ruemena <rue.heart.mena@gmail.com>
2023-06-04 16:46:21 -0500

    t

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+// import { Client } from "discordx"
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv/config');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, c => {
-	console.log(`Ready!!!`);
+client.once(Events.ClientReady, (c: typeof Client) => {
+	console.log(`Ready, logged in as ${c.user.name}`);
 });
 
-if(process.env["TOKEN"] === undefined)
+if (process.env["TOKEN"] === undefined)
 	throw "set client token w/ $TOKEN (including the \"Bot \")"
 else
 	client.login(process.env["TOKEN"]);
