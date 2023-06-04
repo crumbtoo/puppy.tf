0b54bed9544a9c099e945034007d84cf28add483
crumbtoo <crumb@duck.com>
2023-06-04 14:59:58 -0600

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# r(u)ewrite
+
+## possible things maybe from previous puppy and o
+* make build process good (why is package* in my project root fuck off)
+* tf2 logs.tf + ssq
+* (elm? :3) dashboard
+* steam shi t
+* music :D
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`hi ${c.user.tag}`);
+});
+
+if(process.env["TOKEN"] === undefined)
+	throw "set client token w/ $TOKEN (including the \"Bot \")"
+else
+	client.login(process.env["TOKEN"]);
+
