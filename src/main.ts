c971867fba623cf507b3890fcd76c5225ab5157c
crumbtoo <crumb@duck.com>
2023-06-04 20:05:04 -0600

    lavalink

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
-// declare var require: any;
-// declare var process: any;
-
-// import { Client } from "discordx"
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const chalk = require("chalk");
-require('dotenv/config');
-
 import { HighClient } from "./util/objects";
 const highClient = new HighClient(new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]}));
+const { Events, GatewayIntentBits, IntentsBitField } = require('discord.js');
+const { LavaPlayerPlugin } = require("@discordx/plugin-lava-player");
+const { Client, MetadataStorage } = require("discordx");
+const chalk = require("chalk")
+require('dotenv/config');
+
+const lavaPlayerPlugin = new LavaPlayerPlugin({
+	metadata: MetadataStorage.instance,
+});
+
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.GuildMessageReactions,
+		IntentsBitField.Flags.GuildVoiceStates,
+	],
+	plugins: [ lavaPlayerPlugin ],
+});
 
 highClient.client.once(Events.ClientReady, (c: typeof Client) => {
 	console.log(`${chalk.yellow("✨ Ready")}, logged in as ${chalk.blue(c.user.username)}`);
