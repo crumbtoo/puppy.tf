19c4dd62d18df3a5a10c0ac5412d4ab0707986fa
crumbtoo <crumb@duck.com>
2023-06-04 22:21:46 -0600

    cleanup + formatting

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,22 @@ const chalk = require("chalk");
 
 require('dotenv/config');
 
-
-
 const client = new Client({
-	intents: [
-		IntentsBitField.Flags.Guilds,
-		IntentsBitField.Flags.GuildMessageReactions,
-		IntentsBitField.Flags.GuildVoiceStates,
-	],
-//	plugins: [ lavaPlayerPlugin ],
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessageReactions,
+        IntentsBitField.Flags.GuildVoiceStates,
+    ],
 });
+
 const highClient = new HighClient(client);
 
 if(process.env["TOKEN"] === undefined)
-	throw "set client token w/ $TOKEN (including the \"Bot \")";
+    throw "set client token w/ $TOKEN (including the \"Bot \")";
 else
-	highClient.client.login(process.env["TOKEN"]);
-	require("./handlers/slashHandler")(highClient, process.env);
-	require("./handlers/eventHandler")(highClient);
+{
+    highClient.client.login(process.env["TOKEN"]);
+    require("./handlers/slashHandler")(highClient, process.env);
+    require("./handlers/eventHandler")(highClient);
+}
 
